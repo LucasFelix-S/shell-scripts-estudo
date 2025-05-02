276dd7c5cc43c133c350f83b2fcff6a5b08d7663
Lucas Felix Santos <started.lucasfelixsantos@gmail.com>
2025-05-01 22:49:47 -0300

    feat(01-primeiro-script-sh): adicionado para iniciar meus estudos e testar comandos básicos

---
+++ 01-primeiro-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Aprendendo sobre script em shell, utilizando os comandos básicos e iniciais como "echo", "cd", "ls -la" e "mkdir"
+
+echo -e "Meu primeiro script em Shell!\n"
+
+echo -e "Navegando até o diretório de Documentos\n"
+cd /home/lucas/Documentos
+
+echo -e "Criando uma pasta chamada 'teste-lucas'\n"
+mkdir teste-lucas
+
+echo -e "Listando todos os arquivos da minha pasta de Documentos para ver o diretório que criei:\n"
+ls -la
